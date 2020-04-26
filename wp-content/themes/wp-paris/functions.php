9f98f739da9250da2f20b9b1021b7c78e6308b14
Christian <christian@goelette.net>
2020-04-26 14:08:48 +0200

    Thème enfant — déclaration de la taxonomie Destinations

--- wp-content/themes/wp-paris/functions.php
+++ wp-content/themes/wp-paris/functions.php
@@ -16,3 +16,31 @@ add_action( 'wp_enqueue_scripts', function() {
 		'1.0'
 	);
 } );
+
+add_action( 'init', function() {
+	$labels_destinations = array(
+		'name'              => _x( 'Destinations', 'taxonomie nom pluriel', 'to2020' ),
+		'singular_name'     => _x( 'Destination', 'taxonomie nom singulier', 'to2020' ),
+		'search_items'      => __( 'Rechercher une destination', 'to2020' ),
+		'all_items'         => __( 'Toutes les destinations', 'to2020' ),
+		'parent_item'       => __( 'Destination parente', 'to2020' ),
+		'parent_item_colon' => __( 'Destination parente:', 'to2020' ),
+		'edit_item'         => __( 'Éditer la destination', 'to2020' ),
+		'view_item'         => __( 'Afficher la destination', 'to2020' ),
+		'update_item'       => __( 'Mettre à jour la destination', 'to2020' ),
+		'add_new_item'      => __( 'Nouvelle destination', 'to2020' ),
+		'new_item_name'     => __( 'Nouveau nom de destination', 'to2020' ),
+		'menu_name'         => _x( 'Destinations', 'nom dans le menu admin', 'to2020' ),
+		'back_to_items'     => __( 'Retour à la liste des destinations', 'to2020' ),
+	);
+	$args = array(
+		'hierarchical'      => true,  // Pour avoir des checkboxes
+		'labels'            => $labels_destinations,
+		'show_admin_column' => true,  // Afficher dans la colonne dans la liste
+		'show_in_rest'      => true,  // Pour Gutengerg
+	);
+
+	// "Destinations" is a global taxonomy shared amongst most custom post types.
+	register_taxonomy( 'destinations', array( 'post' ), $args );
+} );
+
